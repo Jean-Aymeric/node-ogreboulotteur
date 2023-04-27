d5ef1a6e12a9808a05e339fea15818eb7b53f605
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2023-04-27 23:06:15 +0200

    step 2 done

--- ogreboulotteur.js
+++ ogreboulotteur.js
@@ -1,8 +1,12 @@
-const MAP_HEIGHT = 100
-const MAP_WIDTH = 100
+const MAP_HEIGHT = 20
+const MAP_WIDTH = 20
 const TREE_SPRITE = 'T'
 const GROUND_SPRITE = ' '
+const OGRE_SPRITE = 'ö'
 const FOREST_DENSITY = 20
+const PROMPT = require('prompt-sync')();
+
+let ogrePosition = [MAP_HEIGHT / 2, MAP_WIDTH / 2]
 
 let map = [[]]
 for (let row = 0; row < MAP_HEIGHT; row++) {
@@ -20,10 +24,36 @@ for (let row = 0; row < MAP_HEIGHT; row++) {
     }
 }
 
-for (let row = 0; row < MAP_HEIGHT; row++) {
-    let line = ""
-    for (let column = 0; column < MAP_WIDTH; column++) {
-        line += map[row][column];
+while (true) {
+    for (let row = 0; row < MAP_HEIGHT; row++) {
+        let line = ""
+        for (let column = 0; column < MAP_WIDTH; column++) {
+            if ((ogrePosition[0] === row) && (ogrePosition[1] === column)) {
+                line += OGRE_SPRITE;
+            } else {
+                line += map[row][column];
+            }
+        }
+        console.log(line);
     }
-    console.log(line);
+    let userDirection = 0
+    do {
+        userDirection = PROMPT("");
+    } while (userDirection < 0 || userDirection > 3);
+    switch (userDirection) {
+        case '0':
+            ogrePosition[0] -= 1;
+            break;
+        case '1':
+            ogrePosition[1] -= 1;
+            break;
+        case '2':
+            ogrePosition[0] += 1;
+            break;
+        case '3':
+            ogrePosition[1] += 1;
+            break;
+        default:
+            break;
+    }
 }
