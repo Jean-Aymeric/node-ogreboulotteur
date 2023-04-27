43dbd3e23d19fd0d83724765842348dd9868291b
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2023-04-27 23:29:15 +0200

    step 3 done

--- ogreboulotteur.js
+++ ogreboulotteur.js
@@ -1,5 +1,7 @@
 const MAP_HEIGHT = 20
 const MAP_WIDTH = 20
+const SCREEN_HEIGHT = 10
+const SCREEN_WIDTH = 10
 const TREE_SPRITE = 'T'
 const GROUND_SPRITE = ' '
 const OGRE_SPRITE = 'ö'
@@ -24,12 +26,16 @@ for (let row = 0; row < MAP_HEIGHT; row++) {
     }
 }
 
-while (true) {
-    for (let row = 0; row < MAP_HEIGHT; row++) {
+let running = true;
+while (running) {
+
+    for (let row = ogrePosition[0] - Math.floor(SCREEN_HEIGHT / 2); row < ogrePosition[0] + Math.floor(SCREEN_HEIGHT / 2) + (SCREEN_HEIGHT % 2); row++) {
         let line = ""
-        for (let column = 0; column < MAP_WIDTH; column++) {
+        for (let column = ogrePosition[1] - Math.floor(SCREEN_WIDTH / 2); column < ogrePosition[1] + Math.floor(SCREEN_WIDTH / 2) + (SCREEN_WIDTH % 2); column++) {
             if ((ogrePosition[0] === row) && (ogrePosition[1] === column)) {
                 line += OGRE_SPRITE;
+            } else if ((row < 0) || (row >= MAP_HEIGHT) || (column < 0) || (column >= MAP_WIDTH)) {
+                line += TREE_SPRITE;
             } else {
                 line += map[row][column];
             }
@@ -39,21 +45,34 @@ while (true) {
     let userDirection = 0
     do {
         userDirection = PROMPT("");
-    } while (userDirection < 0 || userDirection > 3);
+    } while (userDirection < 0 || userDirection > 4);
+    let ogreNewPosition = []
+    ogreNewPosition[0] = ogrePosition[0];
+    ogreNewPosition[1] = ogrePosition[1];
     switch (userDirection) {
         case '0':
-            ogrePosition[0] -= 1;
+            ogreNewPosition[0] -= 1;
             break;
         case '1':
-            ogrePosition[1] -= 1;
+            ogreNewPosition[1] -= 1;
             break;
         case '2':
-            ogrePosition[0] += 1;
+            ogreNewPosition[0] += 1;
             break;
         case '3':
-            ogrePosition[1] += 1;
+            ogreNewPosition[1] += 1;
             break;
+        case '4':
+            running = false;
+            break
         default:
             break;
+    }
+    if (map[ogreNewPosition[0]][ogreNewPosition[1]] !== TREE_SPRITE) {
+        ogrePosition[0] = ogreNewPosition[0];
+        ogrePosition[1] = ogreNewPosition[1];
+    }
+     for(let i = 0; i < 50; i++) {
+        console.log('\r\n');
     }
 }
